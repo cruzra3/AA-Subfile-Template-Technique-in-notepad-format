d388ed18a6094524737226c7ddf2feea8903393b
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-11-25 08:24:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AA-Subfile-Template-Technique-in-notepad-format
---
+++ init.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+echo "# AA-Subfile-Template-Technique-in-notepad-format" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/cruzra3/AA-Subfile-Template-Technique-in-notepad-format.git
+git push -u origin main
